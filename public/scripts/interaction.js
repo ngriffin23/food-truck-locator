165ed9e0e8eadfa4efd878850c12df8818a22f56
catcarbonell <hello@catcarbn.com>
2020-03-03 19:09:44 -0800

    Successfully created a close function for the close-icon and cancel buttons

--- public/scripts/interaction.js
+++ public/scripts/interaction.js
@@ -1,16 +1,21 @@
 /* ** DOCUMENT ELEMENTS ** */
-// MENU OPEN/CLOSE
+// GENERAL MULTIPLE ELEMENT/CLASS SELECTOR //
+const anchors = document.querySelectorAll('a');
+const dashBtnArr = document.querySelectorAll('.dash-btn');
+const modalArr = document.querySelectorAll('.modal'); 
+const closeIconArr =  document.querySelectorAll('.close-icon');
+// MENU OPEN/CLOSE //
 const menuOpen = document.querySelector('.dash-menu-open');
 const menuClose = document.querySelector('.dash-menu-close');
 const dashMenu = document.getElementById('dash-menu');
 const findFoodTruck = document.getElementById('find-food-truck');
-// MODAL ELEMENTS
+// MODAL ELEMENTS //
 const openCreate = document.getElementById('open-create');
 const openView = document.getElementById('open-view');
 const openUpdate = document.getElementById('open-update');
 const openSettings = document.getElementById('open-settings');
 const openDelete = document.getElementById('delete-btn');
-// MODALS
+// MODALS //
 const modalCreate = document.getElementById('modal-create');
 const modalView = document.getElementById('modal-view');
 const modalUpdate = document.getElementById('modal-update');
@@ -21,21 +26,17 @@ const closeIcon = document.querySelector('.close-icon');
 const cancelBtn = document.getElementById('cancel-btn');
 const deleteCancel = document.getElementById('delete-cancel');
 
-/* ** FUNCTIONS ** */
-
+/* ======= ======= ======= ======= ======= ======= ======= */
 
 /* ** EVENT LISTENERS  ** */
-
-
-
-// OPEN MENU
+// MENU - OPEN //
 menuOpen.addEventListener('click', function(e) {
     e.stopImmediatePropagation();
     dashMenu.classList.add('slideInLeft');
     dashMenu.classList.remove('slideOutLeft');
     dashMenu.style.display = 'flex';
 });
-// CLOSE MENU
+// MENU - CLOSE //
 menuClose.addEventListener('click', function(e) {
     e.stopImmediatePropagation();
     dashMenu.classList.add('slideOutLeft');
@@ -45,69 +46,70 @@ findFoodTruck.addEventListener('click', function(e){
     dashMenu.classList.add('slideOutLeft');
 });
 
-// MODALS - DELETE - OPEN
+/* ======= ======= ======= ======= ======= ======= ======= */
+
+// MODALS - DELETE - OPEN //
 openDelete.addEventListener('click', function(e){
     e.stopPropagation();
     modalDelete.classList.add('fadeIn');
     modalDelete.classList.remove('fadeOut');
     modalDelete.style.display = 'initial';
 });
-
-// MODALS - CREATE -  OPEN
+// MODALS - CREATE -  OPEN //
 openCreate.addEventListener('click', function(e){
     modalCreate.classList.add('slideInUp');
     modalCreate.classList.remove('slideOutDown');
     modalCreate.style.display = 'initial';
     e.stopPropagation();
 });
-
-// MODALS - EDIT/UPDATE - OPEN
-openUpdate.addEventListener('click', function(){
-    modalUpdate.classList.add('slideInUp');
-    modalUpdate.classList.remove('slideOutDown');
-    modalUpdate.style.display = 'initial';
-});
-
-// MODALS - SETTINGS - OPEN
+// MODALS - SETTINGS - OPEN //
 openSettings.addEventListener('click', function(){
     modalSettings.classList.add('slideInUp');
     modalSettings.classList.remove('slideOutDown');
     modalSettings.style.display = 'initial';
 });
+// MODALS - VIEW POSTS - OPEN //
+openView.addEventListener('click', function(){
+    modalView.classList.add('slideInUp');
+    modalView.classList.remove('slideOutDown');
+    modalView.style.display = 'initial';
+});
+// MODALS - EDIT/UPDATE - OPEN //
+/*openUpdate.addEventListener('click', function(){
+    modalUpdate.classList.add('slideInUp');
+    modalUpdate.classList.remove('slideOutDown');
+    modalUpdate.style.display = 'initial';
+});*/
 
-// MODAL-CLOSE
-// How do I get modal ID on a click event listener?
-// How can I track which link I clicked on
+/* ======= ======= ======= ======= ======= ======= ======= */
 
+// MODALS - CLOSE ICON + CANCEL BTN
 function closeModal(modalId){
-    document.querySelectorAll('.close-icon').forEach(function(thisModal){
-        thisModal.addEventListener('click', function(){
+   closeIconArr.forEach(function(thisModal){
+        thisModal.addEventListener('click', function(e){
             modalId.classList.add('slideOutDown');
-            modalId.classList.remove('slideInUp');
-        
+            e.stopPropagation();
+            e.cancelBubble;
         });
     });
 };
-
-/*function chooseModal(modalId){
-    switch (modalId) {
-        case modalCreate:  
-            closeModal(modalCreate);
-             break;
-        case modalUpdate:
-            closeModal(modalUpdate);
-            break;
-        default:
-          console.log('waiting...');
-      }
-}*/
-
-
-closeIcon.addEventListener('click', function(){
-    modalCreate.classList.add('slideOutDown');
-    modalCreate.classList.remove('slideInUp');
-});
-cancelBtn.addEventListener('click', function(){
-    modalCreate.classList.add('slideOutDown');
-    modalCreate.classList.remove('slideInUp');
-});
+dashBtnArr.forEach(function(thisModalId){
+    thisModalId.addEventListener('click', function(){
+        switch (thisModalId.id) {
+            case 'open-create':  
+                closeModal(modalCreate);
+                break;
+            case 'open-view':
+                closeModal(modalView);
+                break;
+            case 'open-settings':
+                closeModal(modalSettings);
+                break;
+            case 'open-delete':
+                closeModal(modalDelete);
+                break;
+            default:
+                console.log('waiting...');
+            }        
+     });
+ });
